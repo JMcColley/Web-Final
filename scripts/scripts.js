c1754981dbc5c052b136dc4aaad746d033477670
JMcColley <JMcColley@student.neumont.edu>
2018-02-04 21:00:12 -0700

    slideshow

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,20 @@
-window.onload = function(){
 
-    buttonHandler(){
-        
-    }
-    
+var image = document.getElementById("image");
+
+var imageArray = ["images/defaultWatch.jpg", "images/watch1.jpg", "images/watch2.jpg", "images/watch3.jpg"];
+
+var imageIndex = 0;
+
+function changeImage() {
+   image.setAttribute("src",imageArray[imageIndex]);
+   imageIndex++;
+   if (imageIndex >= imageArray.length) {
+      imageIndex = 0;
+   }
+}
+
+window.onload = function() {
+
+   setInterval(changeImage,2000);
+
 }
